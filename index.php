c1a2a2a1d72fccc9b108c6741f1ac542357535b6
yahyabakhtiar <64666316+yahyabakhtiar@users.noreply.github.com>
2020-11-19 09:51:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_GET["FirstName"])) { // show feedback
 } else { // show form
   echo  '
   
-  <form action=''>
+  <form action="">
   First Name: <input type="text" name="FirstName" /><br />
   <input type="submit" />
   
