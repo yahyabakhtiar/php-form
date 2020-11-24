bd6f3ffd9012db867603a68268b1089827ab6ca5
yahyabakhtiar <64666316+yahyabakhtiar@users.noreply.github.com>
2020-11-24 09:24:43 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,14 @@ if (isset($_GET["FirstName"])) { // show feedback
   // echo $_GET["FirstName"];
   
   echo '<pre>';
-  var_dump($_Get);
+  var_dump($_GET);
     echo '<pre>';
+  
 } else { // show form
   echo  '
   <form action="">
-  First Name: <input type="text" name="FirstName" /><br />
+  <p> First Name: <input type="text" name="FirstName" /></p>
+    <p> First Name: <input type="text" name="LastName" /></p>
   <input type="submit" />  
   </form> 
   ';  
