427056cfbae6ad2baf3b02977a98892cda31ec6c
yahyabakhtiar <64666316+yahyabakhtiar@users.noreply.github.com>
2020-11-24 09:15:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 
 
 if (isset($_GET["FirstName"])) { // show feedback
-  echo $_GET["FirstName"];
+  // echo $_GET["FirstName"];
+  
+  echo '<pre>';
+  var_dump($_Get);
+    echo '<pre>';
 } else { // show form
   echo  '
   <form action="">
